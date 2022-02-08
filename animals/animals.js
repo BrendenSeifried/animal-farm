e12eb51c0a15418ead2ad91e9f78abc579dc4ae7
BrendenSeifried <brendenseifried@gmail.com>
2022-02-07 16:40:17 -0800

    learning stuff

--- animals/animals.js
+++ animals/animals.js
@@ -1,12 +1,15 @@
 import { animals } from '../data.js';
 import { findById } from '../utils.js';
+//import { renderListItem } from '../render-utils.js';
 
 const dataStuff = new URLSearchParams(window.location.search);
 const animal = findById(dataStuff.get('id'), animals);
+const anImg = document.getElementById('img');
 
 const name = document.getElementById('name');
 const says = document.getElementById('says');
-
+//console.log('says');
 
 name.textContent = animal.name;
 says.textContent = animal.says;
+anImg.src = `../assets/${animal.type}.svg`;
--- render-utils.js
+++ render-utils.js
@@ -5,7 +5,7 @@ export function renderListItem(animal) {
     div.style.left = animal.left;
 
     const a = document.createElement('a');
-    a.href = `./assets/${animal.id}`;
+    a.href = `./animals/${animal.id}`;
 
     const img = document.createElement('img');
     img.src = `./assets/${animal.type}.svg`;
